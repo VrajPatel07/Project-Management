e62c5a0e2a78e61edd36b5384474f954c517780f
VrajPatel07 <vrajpatel6119@gmail.com>
2025-04-24 11:25:45 +0530

    Add multer for file upload

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,89 @@
 
 
-
-
 export const registerUser = async () => {
     try {
 
-        const {email, username, password, role} = req.body;
+    }
+    catch(error) {
 
-        
+    }
+}
+
+
+export const loginUser = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const logoutUser = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const verifyEmail = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const resendVerificationEmail = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const refreshAccessToken = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const forgotPasswordRequest = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+
+export const changeCurrentPassword = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+
+export const getCurrentUser = async () => {
+    try {
 
     }
     catch(error) {
---
+++ backend/controllers/project.controller.js
@@ -0,0 +1,92 @@
+
+
+export const getProjects = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+
+export const getProjectByID = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+
+export const createProject = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const updateProject = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+} 
+
+
+export const deleteProject = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const addMemberToProject = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const getProjectMembers = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const updateProjectMemberRole = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
+
+
+export const deleteProjectMember = async () => {
+    try {
+
+    }
+    catch(error) {
+
+    }
+}
